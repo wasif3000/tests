c6da3c8c88df3126fa2879603c463f36cc5c5667
wasif3000 <wasif3000@gmail.com>
2024-10-02 16:03:54 +0100

    Add files via upload
    various short tasks completed as part of course

---
+++ various_short_tasks.py
@@ -0,0 +1,44 @@
+# functions
+# def coffee_order (size ,drink ):
+#     print(f"coffee order is a {size} {drink}.")
+
+# coffee_order('Large','latte')
+# coffee_order('medium','cappucino')
+
+
+
+# coffee order with incremental order count
+# order_count=0
+
+
+# def take_order ():
+#     global order_count
+#     order_count=+1
+#     topping=(input("how many pizza? "))
+#     stuffed_crust=(input("what stuffed crust would you like? "))
+#     num_of_pizza=(input("how many pizza would you like? "))
+
+#     print (f"{num_of_pizza} pizza with {topping} and {stuffed_crust}. order count: {order_count}")
+
+
+# take_order()
+# take_order()
+
+
+
+# cash machine
+
+# pin_number=1122
+# balance=100
+
+# def dispence_money ():
+#     pin=int(input("please enter your pin "))
+#     if pin == pin_number:
+#         withdraw =int(input("how much would you like to withdraw £"))
+#         if withdraw <= balance:
+#             print(f"Dispensing £{withdraw}, Your remaining balance is £{balance-withdraw}.")
+#         else:print("Insufficient Funds")
+#     else:print("Incorrect PIN.")
+        
+# dispence_money()
+
